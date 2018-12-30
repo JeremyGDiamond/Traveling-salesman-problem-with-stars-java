1c3005f436f5dfb57986c934f9fc8f07bb00ba6d
JeremyGDiamond <jeremygdiamond@gmail.com>
2018-12-30 13:39:32 -0700

    corrected bad files

--- HW4/src/hw4/HW4.java
+++ HW4/src/hw4/HW4.java
@@ -5,6 +5,14 @@
  */
 package hw4;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.*;
+
+
 /**
  *
  * @author jeremy
@@ -16,6 +24,129 @@ public class HW4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ArrayList<Star> baseStars = new ArrayList<Star>();
+        parser(baseStars);
+        
+        Star CWS; //currently working star
+        Queue<Star> order = new LinkedList<>(); //needed data for preducing a queue
+        
+        order.add(baseStars.get(0)); //block adds sol to order and sets up next nearest star as CWS 
+        baseStars.remove(0);
+        order.add(baseStars.get(0));
+        CWS = baseStars.get(0);
+        baseStars.remove(0);
+        double distanceTotal = 0;
+        int size = baseStars.size(); //needs to be fixed for this to work
+        
+        for (int j = 0; j < size; j++) //places all stars into order and claculates there distances O(n^2)
+        {
+            for (int i = 0; i < baseStars.size(); i++)
+            {
+                baseStars.get(i).distanceCalc(CWS);
+                
+            }
+            Collections.sort(baseStars, new SortByDistance());
+            order.add(baseStars.get(0));
+            CWS = baseStars.get(0);
+            baseStars.remove(0);
+        }
+        
+        while (!(order.isEmpty())) //prints results. is distructive 
+        {
+            distanceTotal += order.peek().distance;
+            if (order.peek().name.isEmpty())
+            {
+                System.out.println("Number of Star: "  + order.peek().number + " Diastance added: " + order.peek().distance );
+            }
+            else
+            {
+                System.out.println("Name of Star: "  + order.peek().name + " Diastance added: " + order.peek().distance );
+            }
+            order.remove();
+        }
+        System.out.println("Diastance of path: " + distanceTotal);
+            
+        
     }
     
+    //based on https://www.mkyong.com/java/how-to-read-and-parse-csv-file-in-java/
+    public static void parser(ArrayList<Star> theStars)
+    {
+        String csvFile = "hygxyz.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        
+        
+        String name;
+        String number;
+        double distFloat = 0;
+        double xFloat = 0;
+        double yFloat = 0;
+        double zFloat = 0;
+        
+        Star tempStar;
+        SortByDistance readin = new SortByDistance();
+        
+        int count = 0;
+        ArrayList testints = new ArrayList();
+        testints.add(1);
+        testints.add(50);
+        testints.add(6);
+        
+        
+        
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            line = br.readLine();
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] nextStar = line.split(cvsSplitBy);
+
+               // System.out.println("Star ID : "+ nextStar[0]+" distance :" + nextStar[9]);
+                
+                distFloat = Float.parseFloat(nextStar[9]);
+                if (distFloat <= 10)
+                {
+                   //System.out.println("Star ID : "+ nextStar[0]+" distance :" + nextStar[9]+ " xpos : "+ nextStar[17] ); 
+                   
+                   count++;
+                   xFloat = Float.parseFloat(nextStar[17]);
+                   yFloat = Float.parseFloat(nextStar[18]);
+                   zFloat = Float.parseFloat(nextStar[19]);
+                   
+                   if (nextStar[6].isEmpty()){
+                       tempStar = new Star(nextStar[0],distFloat,xFloat,yFloat,zFloat);
+                   }
+                   
+                   else{
+                       tempStar = new Star(nextStar[0],nextStar[6],distFloat,xFloat,yFloat,zFloat);
+                   }
+                   
+                  theStars.add(tempStar);
+                }
+
+            }
+            System.out.println(count);
+            Collections.sort(theStars, new SortByDistance());
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+
 }
--- HW4/src/hw4/Star.java
+++ HW4/src/hw4/Star.java
@@ -5,20 +5,24 @@
  */
 package hw4;
 
+import java.util.Comparator;
+
 /**
  *
  * @author jeremy
  */
 public class Star {
+    // data
+     public String number = ""; // used fo string.isEmpty
+     public String name = "";
+     public double distance;
+     public double xPosition;
+     public double yPosition;
+     public double zPosition;
     
-    public int number;
-     public String name;
-     public float distance;
-     public float xPosition;
-     public float yPosition;
-     public float zPosition;
-    
-    public Star (int thenum, String thename, float dist, float x, float y,float z)
+     //constructors
+     
+    public Star (String thenum, String thename, double dist, double x, double y,double z)
     {
         number = thenum;
         name = thename;
@@ -28,7 +32,7 @@ public class Star {
         zPosition = z;
     }
     
-    public Star (int thenum, float dist, float x, float y,float z)
+    public Star (String thenum, double dist, double x, double y,double z)
     {
         number = thenum;
         distance = dist;
@@ -37,5 +41,42 @@ public class Star {
         zPosition = z;
     }
     
+    public void distanceCalc (Star CWS) //implimtation of the 3d distance formula
+    {//CWS = currently working star
+        double diffX = (this.xPosition)-(CWS.xPosition);//abs value not needed because they will be squared
+        double diffY = (this.yPosition)-(CWS.yPosition);
+        double diffZ = (this.zPosition)-(CWS.zPosition);
+        
+        diffX = diffX*diffX;
+        diffY = diffY*diffY;
+        diffZ = diffZ*diffZ;
+        
+        double squSum = diffX+ diffY + diffZ;
+        
+        this.distance = Math.sqrt(squSum);
+    }
+    
+    
+}
+
+class SortByDistance implements Comparator<Star> //this class allows one to use native java sort on ADT star  
+{
+    
+    public int compare(Star a, Star b)
+    {
+        
+        if(a.distance > b.distance){
+            return 1;
+        }
+        
+        else if (a.distance < b.distance)
+        {
+            return -1;
+        }
+        
+        else{
+            return 0;
+        }
+    }
     
 }
